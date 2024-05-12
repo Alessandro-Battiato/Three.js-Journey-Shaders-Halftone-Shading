835679dce58a5306c1e87a6e473ead2799b97343
alessandrobattiato <alessandroDev@outlook.it>
2024-05-12 12:01:51 +0200

    feat: implemented correct grid like dot effects with smaller dots at the beginning and bigger dots through the end

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -34,13 +34,19 @@ void main()
 
     // Halftone
     float repetitions = 50.0;
+    vec3 direction = vec3(0.0, -1.0, 0.0);
+    float low = - 0.8;
+    float high = 1.5;
+
+    float intensity = dot(normal, direction); // dot product
+    intensity = smoothstep(low, high, intensity);
 
     vec2 uv = gl_FragCoord.xy / uResolution.y; // now both x and y of fragCoords are divided by uResolution's y and thus fixes the squares issue
     uv *= repetitions; // how many cells (or squares) we have vertically on each mesh
     uv = mod(uv, 1.0);
 
     float point = distance(uv, vec2(0.5)); // render a Dot at the center of each cell
-    point = 1.0 - step(0.5, point);
+    point = 1.0 - step(0.5 * intensity, point);
 
     // Final color
     gl_FragColor = vec4(point, point, point, 1.0);
