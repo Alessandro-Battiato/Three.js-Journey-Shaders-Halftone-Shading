dfe293479b35fcf087fcc26abe3e6002f1205301
alessandrobattiato <alessandroDev@outlook.it>
2024-05-12 11:31:55 +0200

    fix: fixed frag coordinates issue thus now rendering colors for materials

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -1,4 +1,5 @@
 uniform vec3 uColor;
+uniform vec2 uResolution;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -32,7 +33,7 @@ void main()
     color *= light;
 
     // Halftone
-    vec2 uv = gl_FragCoord.xy;
+    vec2 uv = gl_FragCoord.xy / uResolution;
 
     // Final color
     gl_FragColor = vec4(uv, 1.0, 1.0);
